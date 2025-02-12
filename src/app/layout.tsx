279eda855f77b1bf0bcfb021b3a6a5ad602977bd
jsh612 <urijsh612@gmail.com>
2025-02-13 08:55:11 +0900

    etc: favicon 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Util Web",
   description: "Utility Web Application",
+  icons: {
+    icon: "/favicon.svg",
+  },
 };
 
 export default function RootLayout({
